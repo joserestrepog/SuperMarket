db27d19cdd90a5184a103816f70e217830f786c1
Jose Luis Restrepo Gesamá <jose1.restrepo@ucp.edu.co>
2022-05-04 01:07:09 -0500

    Consultas.

--- Modelo/conexion.php
+++ Modelo/conexion.php
@@ -3,15 +3,20 @@
     $conn = pg_connect("host=192.168.1.150 port=5432 dbname=SuperMarket user=postgres password=postgres") or die('No se ha podido conectar: ' . pg_last_error());
 
     if ($conn){
-        echo "Conexión exitosa";
+        echo "Conexión exitosa"."<br>"."<br>";
     }
     $schema = '"ScSuperMarket"';
-    $query = "SELECT nombre FROM ".$schema.".usuario;";
+    $query = "SELECT id_usuario, nombre, rol, fk_id_sucursal, username, contrasena FROM ".$schema.".usuario;";
     $consulta = pg_query($conn,$query);
     if($consulta){
-        while($obj=pg_fetch_object($consulta)){
-            echo $obj->nombre."<br>";
-        }
+    while($obj=pg_fetch_object($consulta)){
+            echo $obj->id_usuario."<br>";
+        echo $obj->nombre."<br>";
+        echo $obj->rol."<br>";
+        echo $obj->fk_id_sucursal."<br>";
+        echo $obj->username."<br>";
+        echo $obj->contrasena."<br>"."<br>";
+    }
     }
            
  
