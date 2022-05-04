1c4312142d0db235937ec6f9da3020504bbcf237
Angie Carolina Gómez Garcés <angie.gomez@ucp.edu.co>
2022-05-04 01:37:18 -0500

    Singleton
    Primera prueba

--- Modelo/Singleton.php
+++ Modelo/Singleton.php
@@ -1,64 +1,21 @@
 <?php
 
-
-
-class Singleton
-{
-    
-    private static $instances = [];
-
-    protected function __construct() { }
-
-    protected function __clone() { }
-
-    public function __wakeup()
-    {
-        throw new \Exception("Cannot unserialize a singleton.");
-    }
-
-    /**
-     * This is the static method that controls the access to the singleton
-     * instance. On the first run, it creates a singleton object and places it
-     * into the static field. On subsequent runs, it returns the client existing
-     * object stored in the static field.
-     *
-     * This implementation lets you subclass the Singleton class while keeping
-     * just one instance of each subclass around.
-     */
-    public static function getInstance(): Singleton
-    {
-        $cls = static::class;
-        if (!isset(self::$instances[$cls])) {
-            self::$instances[$cls] = new static();
+    public function prueba(){
+        $schema = '"ScSuperMarket"';
+        $query = "SELECT id_usuario, nombre, rol, fk_id_sucursal, username, contrasena FROM ".$schema.".usuario;";
+        $consulta = pg_query(Connetion::getInstance(),$query);
+        if($consulta){
+            while($obj=pg_fetch_object($consulta)){
+                    echo $obj->id_usuario."<br>";
+                echo $obj->nombre."<br>";
+                echo $obj->rol."<br>";
+                echo $obj->fk_id_sucursal."<br>";
+                echo $obj->username."<br>";
+                echo $obj->contrasena."<br>"."<br>";
+            }
         }
-
-        return self::$instances[$cls];
     }
-
-    /**
-     * Finally, any singleton should define some business logic, which can be
-     * executed on its instance.
-     */
-    public function someBusinessLogic()
-    {
-        // ...
-    }
-}
-
-/**
- * The client code.
- */
-function clientCode()
-{
-    $s1 = Singleton::getInstance();
-    $s2 = Singleton::getInstance();
-    if ($s1 === $s2) {
-        echo "Singleton works, both variables contain the same instance.";
-    } else {
-        echo "Singleton failed, variables contain different instances.";
-    }
-}
-
-clientCode();
+    
+    prueba();
 
 ?>
--- Modelo/conexion.php
+++ Modelo/conexion.php
@@ -1,23 +1,28 @@
 <?php
-   
-    $conn = pg_connect("host=192.168.1.150 port=5432 dbname=SuperMarket user=postgres password=postgres") or die('No se ha podido conectar: ' . pg_last_error());
-
-    if ($conn){
-        echo "Conexión exitosa"."<br>"."<br>";
+    class Connection
+    {
+        private function __construct()
+        {
+            $conn = pg_connect("host=192.168.1.150 port=5432 dbname=SuperMarket user=postgres password=postgres") or die('No se ha podido conectar: ' . pg_last_error());
+            if ($conn){
+                echo "Conexión exitosa"."<br>"."<br>";
+            }
+        }
+    
+        public static function getInstance()
+        {
+            static $instance = null;
+            if (null === $instance) {
+                $instance = new static();
+            } else {
+                echo 'El objeto ya existe, no puedes volver a crearlo <br />';
+            }
+    
+            return $instance;
+        }
     }
-    $schema = '"ScSuperMarket"';
-    $query = "SELECT id_usuario, nombre, rol, fk_id_sucursal, username, contrasena FROM ".$schema.".usuario;";
-    $consulta = pg_query($conn,$query);
-    if($consulta){
-    while($obj=pg_fetch_object($consulta)){
-            echo $obj->id_usuario."<br>";
-        echo $obj->nombre."<br>";
-        echo $obj->rol."<br>";
-        echo $obj->fk_id_sucursal."<br>";
-        echo $obj->username."<br>";
-        echo $obj->contrasena."<br>"."<br>";
-    }
-    }
+    
+    
            
  
 ?>
